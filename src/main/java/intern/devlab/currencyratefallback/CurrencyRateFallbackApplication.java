e1b8bbbb480b7e961b0b43f5b1478b77706c112d
Turqut <turqutismayil007@gmail.com>
2025-11-17 11:00:27 +0400

    Initial Spring Boot project setup

---
+++ src/main/java/intern/devlab/currencyratefallback/CurrencyRateFallbackApplication.java
@@ -0,0 +1,13 @@
+package intern.devlab.currencyratefallback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyRateFallbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyRateFallbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/intern/devlab/currencyratefallback/CurrencyRateFallbackApplicationTests.java
@@ -0,0 +1,13 @@
+package intern.devlab.currencyratefallback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyRateFallbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
