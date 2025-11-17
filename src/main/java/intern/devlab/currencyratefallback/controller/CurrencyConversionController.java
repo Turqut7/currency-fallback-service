d40562be18b0a908489f5f01fef1ffc22b647895
Turqut <turqutismayil007@gmail.com>
2025-11-17 11:47:28 +0400

    Add /ping test endpoint

--- src/main/java/intern/devlab/currencyratefallback/controller/CurrencyConversionController.java
+++ src/main/java/intern/devlab/currencyratefallback/controller/CurrencyConversionController.java
@@ -1,4 +1,13 @@
 package intern.devlab.currencyratefallback.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
 public class CurrencyConversionController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "Service is running!";
+    }
 }
